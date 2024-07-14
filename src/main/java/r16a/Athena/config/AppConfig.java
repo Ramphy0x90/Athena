878900e39b6000e290f16a96f7b3aa7fd2e34910
ramphy0x90 <ramphy11@hotmail.com>
2024-07-14 09:07:27 +0200

    Update AppConfig.java

--- src/main/java/r16a/Athena/config/AppConfig.java
+++ src/main/java/r16a/Athena/config/AppConfig.java
@@ -1,11 +1,23 @@
 package r16a.Athena.config;
 
+import lombok.Getter;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
 @Configuration
 public class AppConfig {
+    @Value("${app.base-url}")
+    private String appBaseUrl;
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Value("${app.is-production}")
+    private boolean isProduction;
+
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
