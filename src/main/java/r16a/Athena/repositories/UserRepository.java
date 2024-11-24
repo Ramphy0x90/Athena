fa94b3ca8f07374822d688f988f11441b6ce7cf0
ramphy0x90 <ramphy11@hotmail.com>
2024-11-24 11:53:13 +0100

    Update UserRepository.java

--- src/main/java/r16a/Athena/repositories/UserRepository.java
+++ src/main/java/r16a/Athena/repositories/UserRepository.java
@@ -13,6 +13,16 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.email = :user OR u.userName = :user")
     Optional<User> findByEmailOrUserName(@Param("user") String user);
 
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :user OR u.userName = :user")
-    boolean existsByEmailOrUserName(@Param("user") String user);
+    @Query("SELECT u FROM User u WHERE (u.email = :user OR u.userName = :user) AND u.internal = TRUE")
+    Optional<User> findAdminByEmailOrUserName(@Param("user") String user);
+
+    @Query("SELECT u " +
+            "FROM User u LEFT JOIN u.clients c " +
+            "WHERE (u.email = :user OR u.userName = :user) AND c.id = :clientId")
+    Optional<User> findByEmailOrUserNameAndClientId(@Param("user") String user, @Param("clientId") Integer clientId);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END " +
+            "FROM User u LEFT JOIN u.clients c " +
+            "WHERE (u.email = :user OR u.userName = :user) AND c.id = :clientId")
+    boolean existsByEmailOrUserNameAncClientId(@Param("user") String user, @Param("clientId") Integer clientId);
 }
